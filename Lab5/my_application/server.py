f83205d2ce789e0e26c8ce6944c3d03c607f2e98
Lorna Keogh <C13740351@mydit.ie>
2015-10-14 15:30:15 +0100

    Update server.py

--- Lab5/my_application/server.py
+++ Lab5/my_application/server.py
@@ -3,19 +3,19 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    return "Hello World!"
+    return "Hello World!\n"
     
 @app.route("/")
 def index():
-    return "Index Page"
+    return "Index File/n"
     
 @app.route("/user/paul")
 def user():
-    return "User Paul"
+    return "User Paul/n"
 
 @app.route("/post/80")
 def post():
-    return "Post 80"
+    return "Post 80/n"
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0")
