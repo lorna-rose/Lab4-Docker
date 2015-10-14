42bb54a1a220db361b4ef930cb3855db14e57f52
Lorna Keogh <C13740351@mydit.ie>
2015-10-14 15:50:37 +0100

    Update server.py

--- Lab5/my_application/server.py
+++ Lab5/my_application/server.py
@@ -19,5 +19,5 @@ def post():
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0")
-    app.run(host='0.0.0.0',port=8080,debug=True)
-
+    #app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
